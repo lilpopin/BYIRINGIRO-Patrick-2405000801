15041b1eb53efc6385485c0db6f5db63757b278c
lilpopin <163481101+lilpopin@users.noreply.github.com>
2025-04-13 13:17:01 +0200

    Add files via upload

---
+++ string.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<font color="green"><h1>The main body of Book </h1></font>
+<hr>
+<?php
+$sentence ="The main body of Book";
+echo strtolower($sentence);
+echo strtoupper($sentence);
+echo str_replace ("Book", "College",$sentence);
+ echo strlen("$sentence");
+ echo $sentence [5]="5"
+?> 
+
+</body>
+</html>
